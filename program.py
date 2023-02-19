cb0c7654020c8faa72d39063933f13303712bd28
kater74 <patryk@fachowe-tlumaczenia.pl>
2023-02-19 13:30:52 +0000

    Added a final message for >120 yo

--- program.py
+++ program.py
@@ -1,20 +1,18 @@
 # v.1.0 dzialajacej aplikacji
 
 wiek = input("Podaj wiek uzytkownika: ")
-# sprawdzenie czy wiek jest liczba calkowita
+#sprawdzenie czy wiek jest liczba calkowita
 if wiek.isdigit() == False:
     exit("Wiek musi byc liczba calkowita. Zamykam aplikacje")
 wiek=int(wiek)
-if wiek >= 18 and wiek<=40:
-    print("Witamy w apce. Mozesz kupowac u nas alkohol")
+if wiek>=18 and wiek<=40:
+    print("Witam w apce. Mozesz u nas kupowac alkohol")
 elif wiek>40 and wiek<=120:
-    print("Witamy w apce. Mozesz kupowac u nas alkohol")
-    print("Prosze korzystaj z produktu z umiarem")
+    print("Witam w apce. Mozesz u nas kupowac alkohol")
+    print("Korzystaj z produktow z umiarem")
 elif wiek>120:
-    print("Witamy w apce. Mozesz kupowac u nas alkohol")
-    print("W twoim wieku alkohol? Masz zdrowie...")
+    print("Witam w apce. Mozesz u nas kupowac alkohol")
+    print("Ale w tym nie wieku lepiej tego jednak nie robic...")
 else:
-    a = 18 - wiek
-    print("Jestes za mlody/a na alkohol. Zapraszamy za",a,"lat/a")
-    exit
-
+    exit("Jestes za mlody/a na alkohol. Zapraszamy na disney.com")
+    
